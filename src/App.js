80aa005100f5d21eb231d0083447a6e84e32f4db
David <davidgrin1337@gmail.com>
2022-04-05 09:49:31 +0200

    deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { ColorPicker, useColor } from "react-color-palette";
+import "react-color-palette/lib/css/styles.css";
+
+export const App = () => {
+  const [color, setColor] = useColor("hex", "#c14229ff");
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container">
+      
+    <table>
+    <tr>
+      <th>
+
+      <div style={{background: "#000000a2",padding: "20px",margin: "20px", borderRadius:"12px",}}>
+      <h2><b>RGBA NORMALIZED</b> "{(parseFloat(color.rgb.r)/255).toFixed(3)} {(parseFloat(color.rgb.g)/255).toFixed(3)} {(parseFloat(color.rgb.b)/255).toFixed(3)} {parseFloat(color.rgb.a).toFixed(2)}"</h2>
+            
+        <table>
+        <tr>
+          <th>
+            <div> 
+              <ColorPicker width={250} height={200} color={color} alpha onChange={setColor} hideRGB hideHSV dark />
+            </div>
+          </th>
+          <th>
+            <div style={{background: color.hex, paddingTop: "50px", margin: "auto", borderRadius:"12px", height:320, width:250}}>
+              
+            </div>
+          </th>
+          <th>
+            </th>
+        </tr>
+        </table>
+      </div>
+      </th>
+      <th>
+      <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+      <a href="https://rustplugins.net/support"><img src={require('./support2.png')} /></a>
+      </th>
+      </tr>
+      </table>
+  </div>
   );
-}
+};
+
+
 
 export default App;
